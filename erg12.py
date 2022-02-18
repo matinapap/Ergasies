8cba22f8a350c40a395344aa9e5ba4e98f50c4cf
matinapap <92823316+matinapap@users.noreply.github.com>
2022-02-18 12:09:44 +0200

    Update erg12.py

--- erg12.py
+++ erg12.py
@@ -5,19 +5,17 @@ from urllib.request import Request, urlopen
 req = Request('https://drand.cloudflare.com/public/latest', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0'})
 data = urlopen(req).read()
 
-
-#listdata=list(data)
-data2=str(data)                         #metatrepw se string 
+data2=str(data)                         #μετατρέπω σε string 
 randomness=data2.split(',')
 round1=randomness[0].replace("round","")
-round1=round1.replace("b'{","")             #afairw osous xarakthres den xreiazomai apo to round
+round1=round1.replace("b'{","")             #αφαιρώ όσους χαρακτήρες δεν χρειαζομαι από το round
 round1=round1.replace('"":',"")
 round1=int(round1)
 final=randomness[1].replace("randomness","")
 final=final.replace(":","")
-final=final.replace('"',"")                             #afairw osous xarakthres den xreiazomai apo to randomness
+final=final.replace('"',"")                             #αφαιρώ όσους χαρακτήρες δεν χρειάζομαι από το randomness
 binum = ''.join(format(ord(i), 'b') for i in final)
-b01=binum                                               #krataw tous arithmous 
+b01=binum                                               #κρατάω τους αριθμούς 
 
 for i in range (1,100):
     round1=round1-1
@@ -28,14 +26,14 @@ for i in range (1,100):
     final=randomness[1].replace("randomness","")
     final=final.replace(":","")
     final=final.replace('"',"")
-    binum = ''.join(format(ord(i), 'b') for i in final)     #metatrepw se binary
+    binum = ''.join(format(ord(i), 'b') for i in final)     #μετατρέπω σε binary
     b01+=binum
-b01=list(b01)                   #vazw 3exwrista kathe stoixeio 01 se mia lista gia na mporw na ta metrhsw 
+b01=list(b01)                   #βάζω ξεχωριστά κάθε στοιχείο 01 σε μια λίστα για να μπορώ να τα μετρήσω 
 
 pl0=[0]
 j=0
 for i in range (len(b01)):
-    if b01[i]!='1':                             #vriskw to plhthos 
+    if b01[i]!='1':                             #βρίσκω το πλήθος 
         pl0[j]+=1
     else:
         pl0.append(0)
