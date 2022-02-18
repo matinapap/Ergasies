99432d5147fe22b939e3525f4b388057aa64e307
matinapap <92823316+matinapap@users.noreply.github.com>
2022-02-18 12:26:56 +0200

    Update erg6.py

--- erg6.py
+++ erg6.py
@@ -11,9 +11,9 @@ def print_board(pg,ps,axg,axs,vag,vas):
               ["  ","  ","  ","  ","  ","  ","  ","  "],
               ["  ","  ","  ","  ","  ","  ","  ","  "],
               ["  ","  ","  ","  ","  ","  ","  ","  "]]
-    skakiera[int(pg-1)][int(ps-1)]="lp"                    #leukos purgos
-    skakiera[int(axg-1)][int(axs-1)]="la"                   #leukos axiomatikos
-    skakiera[int(vag-1)][int(vas-1)]="mv"                   #mavrh vasilissa
+    skakiera[int(pg-1)][int(ps-1)]="lp"                    #λευκός πύργος
+    skakiera[int(axg-1)][int(axs-1)]="la"                   #λευκός αξιωματικός
+    skakiera[int(vag-1)][int(vas-1)]="mv"                   #μαύρη βασίλισσα
     return skakiera
 
 def grammes():
@@ -35,10 +35,10 @@ def pyrgos(pg,ps,vag,vas):
 
 def axiomatikos(axg,axs,vag,vas):
     kerdise=False
-    if ((axs+axg)%2==1)&((vas+vag)%2==1):       #mavra tetragwna
+    if ((axs+axg)%2==1)&((vas+vag)%2==1):       #μάυρα τετράγωνα
         if ((axs-axg==1)&(vas-vag==1))or((axg-axs==1)&(vag-vas==1))or((axg-axs==3)&(vag-vas==3))or((axs-axg==3)&(vas-vag==3))or((axg-axs==5)&(vag-vas==5))or((axg-axs==5)&(vag-vas==5))or((axs+axg==11)&(vas+vag==11))or((axs+axg==3)&(vas+vag==3))or((axs+axg==5)&(vas+vag==5))or((axs+axg==7)&(vas+vag==7))or((axs+axg==9)&(vas+vag==9))or((axs+axg==13)&(vas+vag==13))or((axs+axg==15)&(vas+vag==15)):
             kerdise=True
-    else:                                       #lefka tetragwna
+    else:                                       #λευκά τετράγωνα
         if ((axs-axg==0)&(vas-vag==0))or((axg-axs==2)&(vag-vas==2))or((axg-axs==4)&(vag-vas==4))or((axg-axs==6)&(vag-vas==6))or((axs-axg==2)&(vas-vag==2))or((axs-axg==4)&(vas-vag==4))or((axs-axg==6)&(vas-vag==6))or((axs+axg==4)&(vas+vag==4))or((axs+axg==6)&(vas+vag==6))or((axs+axg==8)&(vas+vag==8))or((axs+axg==10)&(vas+vag==10))or((axs+axg==12)&(vas+vag==12))or((axs+axg==14)&(vas+vag==14)):
             kerdise=True
     return kerdise
@@ -46,8 +46,8 @@ def axiomatikos(axg,axs,vag,vas):
 p1=0
 p2=0
 paizei=True
-#prwtos paikths false 
-#deuteros paikths true 
+#πρώτος παίκτης false 
+#δεύτερος παίκτης true 
 for i in range (100):
     if (i+1)%2==0:
         paizei=True
@@ -59,7 +59,7 @@ for i in range (100):
     axs=sthles()
     while (pg==axg)&(ps==axs):
         axg=grammes()
-        axs=sthles()                        #vriskw thn grammh kai thn sthlh pou einai kathe pioni kai kanw elegxw wste na einai diaforetikh h thesh kathe pioniou 
+        axs=sthles()                        #βρίσκω την γραμμή και την στήλη που είνα κάθε πιόνι και κάνω έλεγχο ώστε να είναι διαφορετική η θέση κάθε πιονιού 
     vag=grammes()
     vas=sthles()
     while ((pg==vag)&(ps==vas))or((axg==vag)&(axs==vas)):
@@ -67,17 +67,17 @@ for i in range (100):
         vas=sthles()
     skakiera=print_board(pg,ps,axg,axs,vag,vas)
     for i in range (8):                         
-        print(skakiera[i])                          #ektupwnei kathe fora thn skakiera 
+        print(skakiera[i])                          #εκτυπώνει κάθε φορά την σκακιέρα 
     print(" ")
     if paizei==False:
-        kerdise=pyrgos(pg,ps,vag,vas)           #gia ton pirgo 
+        kerdise=pyrgos(pg,ps,vag,vas)           #για τον πύργο
         if kerdise==True:
             p1+=1
-        kerdise=axiomatikos(axg,axs,vag,vas)  #gia ton axiomatiko
+        kerdise=axiomatikos(axg,axs,vag,vas)  #για τον αξιοματικό
         if kerdise==True:
             p1+=1
     else:
-        kerdise=pyrgos(pg,ps,vag,vas)           #gia thn vassilisa
+        kerdise=pyrgos(pg,ps,vag,vas)           #για την βασίλισσα
         if kerdise==False:
             kerdise=axiomatikos(pg,ps,vag,vas)
         if kerdise==True:
